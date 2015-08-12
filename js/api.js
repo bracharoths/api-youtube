ba62560ce4ce3cfcd561c9c1795d697a1ab5e02a
bracharoths <bracha.b.rothschild@gmail.com>
2015-08-11 18:18:53 -0600

    second commit

--- js/api.js
+++ js/api.js
@@ -6,24 +6,48 @@ $(function(){
   });
 });
 
-function getRequest(searchTerm){
+function getRequest(search){
   var params = {
-    part: 'snipet',
+    part: 'snippet',
     key: 'AIzaSyChbz3MFtKY3RooOSLAZrqXnjppHlHEWTo',
-    q: 'json'
+    type: 'video',
+    q: search
   };
+
   url = 'https://www.googleapis.com/youtube/v3/search';
 
   $.getJSON(url, params, function(data){
-    showResults(data.Search);
+    // showResults(data.Search);
+    console.log(data)
   });
 }
 
-function showResults(results){
-  var html = "";
-  $.each(results, function(index,value){
-    html += '<p>' + value.Title + '</p>';
-    console.log(value.Title);
-  });
-  $('#search-results').html(html);
-}
+// function showResults(results){
+//   var str = JSON.stringify(response.result)
+//   $.each(results, function(index,value){
+//     html += '<p>' + value.Title + '</p>';
+//     console.log(value.Title);
+//   });
+//   $('#search-results').html(html);
+// }
+
+// $(function(){
+// console.log('ready');
+//   $.getJSON('https://www.googleapis.com/youtube/v3/search', {
+//   	key: 'AIzaSyChbz3MFtKY3RooOSLAZrqXnjppHlHEWTo',
+//   	part: 'snippet',
+//   	type: 'video'
+//   }, 
+//   function(data){
+
+//   	console.log(data)
+//   });
+
+//   $.getJSON('https://www.googleapis.com/youtube/v3/search?key=AIzaSyChbz3MFtKY3RooOSLAZrqXnjppHlHEWTo&part=snippet&type=video', function(data){
+//     // myData = data.Search;
+//     // $.each(myData, function(index,value){
+//     // console.log(value.Title);
+//     // });
+//   console.log(data);
+//   });
+// });
